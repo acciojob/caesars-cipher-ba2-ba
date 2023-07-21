bf9e5b72476870d1dbb5d0a74de280a71d6c49a9
ba2-ba <112476436+ba2-ba@users.noreply.github.com>
2023-07-21 23:01:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -31,9 +31,27 @@ const lookup = {
   ",": ",",
 };
 
-function rot13(encodedStr) {
-  let decodedArr = []; // Your Result goes here
-  // Only change code below this line
+
+
+	function rot13(str) { // LBH QVQ VG!
+  var arr = [];
+
+  for (var i = 0; i < str.length; i++) {
+    if (str.charCodeAt(i) < 65 || str.charCodeAt(i) > 90) {
+      arr.push(str.charAt(i));
+      console.log(arr);
+    } else if (str.charCodeAt(i) > 77) {
+      arr.push(String.fromCharCode(str.charCodeAt(i) - 13));
+    } else {
+      arr.push(String.fromCharCode(str.charCodeAt(i) + 13));
+    }
+  }
+
+  return arr.join("");
+}
+
+// Change the inputs below to test
+rot13("SERR CVMMN!");
 
   return; //return decodedArr
 }
